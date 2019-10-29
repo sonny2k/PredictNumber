ac37e0357f9e7fcf9b88aa4f861a3b6211d52584
pmtruong24 <pmtruong2472@gmail.com>
2019-10-29 15:27:02 +0700

    finished

---
+++ Magician/Magician/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            int A = 0, B = 100;
+            int c = 1;
+            while(B > A + 1)
+            {
+                int C = (A + B) / 2;
+                Console.WriteLine("Q{0}: Is your number greater than {1}?", c, C);
+                c++;
+                answer = Console.ReadLine();
+                if (answer == "Y")
+                    A = C + 1;
+                if (answer == "N")
+                    B = C;
+            }
+            Console.WriteLine("Q{0}: Is your number {1}", c, A);
+            answer = Console.ReadLine();
+            if (answer == "Y")
+                Console.WriteLine("Your number is " + A);
+            if (answer == "N")
+                Console.WriteLine("Your number is " + B);
+        }
+    }
+}
